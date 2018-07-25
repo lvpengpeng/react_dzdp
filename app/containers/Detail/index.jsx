5341d71b935efed5b5203c18fa80b878d22134ef
吕鹏 <731010527@qq.com>
2018-07-25 11:59:34 +0800

    封装商家详情接口和下拉加载接口,放在fetch的detai文件夹里,添加mock数据房租mock文件夹的detail里

--- app/containers/Detail/index.jsx
+++ app/containers/Detail/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
+import Header  from '../../components/Header'
 class Detail extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
     render() {
+        const id = this.props.params.id
+        console.log(id,6)
         return (
             <div>
+                <Header data="商家详情"/>
                 <h1>Detail</h1>
             </div>
         )
